04a16660406af24ecfe5fb28fe4537c0437b981c
Oshomo Oforomeh <oshomo.oforomeh@konga.com>
2016-10-03 17:12:31 +0100

    Removed bootstrap nav Added materialCss nav

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,68 +6,27 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>surveyFor || {{ $title or 'Welcome' }}</title>
         <link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
         <link href="{!! asset('css/app.css') !!}" media="all" rel="stylesheet" type="text/css" />
     </head>
 
     <body>
         <div id="blade_content">
-            <div>
-                <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-                    <div class="container">
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                            <a class="navbar-brand" href="/">SurveyFor</a>
-                        </div>
-                        <div class="collapse navbar-collapse">
-                            <ul class="nav navbar-nav navbar-right">
-                                <li class="active"><a href="#">Home</a></li>
+            @if (!Auth::guest())
+                <nav>
+                    <div class="nav-wrapper">
+                        <a href="#!" class="brand-logo">SurveyFor</a>
+                        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+                        <ul class="right hide-on-med-and-down">
+                            <li><a class="dropdown-button" href="#!" data-activates="dropdown1">{{ Auth::user()->name }}<i class="material-icons right">arrow_drop_down</i></a></li>
+                            <ul id="dropdown1" class="dropdown-content">
+                                <li><a href="{{ url('/logout') }}">Logout</a></li>
                             </ul>
-                        </div><!--/.nav-collapse -->
+                        </ul>
                     </div>
-                </div>
-                <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-                    <div class="container">
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                            <a class="navbar-brand" href="/">SurveyFor</a>
-                        </div>
-                        <div class="collapse navbar-collapse">
-                            <ul class="nav navbar-nav navbar-right">
-                                @if (Auth::guest())
-                                    <li><a href="{{ url('/login') }}">Login</a></li>
-                                    <li><a href="{{ url('/register') }}">Register</a></li>
-                                @else
-                                    <li class="dropdown">
-                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                            {{ Auth::user()->name }} <span class="caret"></span>
-                                        </a>
-
-                                        <ul class="dropdown-menu" role="menu">
-                                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                                        </ul>
-                                    </li>
-                                @endif
-                            </ul>
-                        </div><!--/.nav-collapse -->
-                    </div>
-                </div>
-            </div>
-
-            <div>
-                @yield('slider')
-            </div>
-
+                </nav>
+            @endif
             <div>
                 @yield('content')
             </div>
